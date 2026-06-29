82cd15edf282824ae963d2ddb0320fa5c20bac02
santi <santiberterano@gmail.com>
2026-06-28 22:01:34 -0300

    fix: remove BOM from JS files

--- src/components/modulos/ExplosionInsumos.js
+++ src/components/modulos/ExplosionInsumos.js
@@ -1,4 +1,4 @@
-﻿import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import * as XLSX from 'xlsx'
 import { supabase } from '../../supabaseClient'
 
